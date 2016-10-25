a8b8358d31b0ed24e8150867c05d3cebc2043060
zhaoxu <330868352@qq.com>
2016-10-25 18:11:17 +0800

    加了一个划动返回上一页面的功能

--- MyProject/app/src/main/java/myapp/example/com/myproject/MainActivity.java
+++ MyProject/app/src/main/java/myapp/example/com/myproject/MainActivity.java
@@ -1,13 +1,15 @@
 package myapp.example.com.myproject;
 
-import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.bumptech.glide.Glide;
 import com.github.yoojia.inputs.AndroidNextInputs;
 import com.github.yoojia.inputs.StaticScheme;
+import com.source.activity.BaseLibActivity;
 import com.source.util.JsonUtil;
 import com.source.widget.image.config.ViewSimpleTarget;
 
@@ -16,19 +18,39 @@ import java.util.List;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
+import butterknife.OnClick;
 import jp.wasabeef.glide.transformations.CropCircleTransformation;
+import me.imid.swipebacklayout.lib.SwipeBackLayout;
 
-public class MainActivity extends Activity {
+public class MainActivity extends BaseLibActivity {
+    private static final int VIBRATE_DURATION = 20;
     @Bind(R.id.image)
     ImageView imageview;
     @Bind(R.id.tv)
     TextView tv;
 
+
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+    protected void initView(Bundle savedInstanceState) {
         setContentView(R.layout.content_main);
         ButterKnife.bind(this);
+
+//        mSwipeBackLayout.addSwipeListener(new SwipeBackLayout.SwipeListener() {
+//            @Override
+//            public void onScrollStateChange(int state, float scrollPercent) {
+//
+//            }
+//
+//            @Override
+//            public void onEdgeTouch(int edgeFlag) {
+//                vibrate(VIBRATE_DURATION);
+//            }
+//
+//            @Override
+//            public void onScrollOverThreshold() {
+//                vibrate(VIBRATE_DURATION);
+//            }
+//        });
 
         List<Person> list=new ArrayList();
         for(int i=0;i<10;i++) {
@@ -59,11 +81,11 @@ public class MainActivity extends Activity {
         final AndroidNextInputs inputs = new AndroidNextInputs(this);
         inputs.add(tv, StaticScheme.ChineseMobile());
         if(!inputs.test()){
-             //
+            //
         }
 
 
-       // imageview= (ImageView) findViewById(R.id.image);
+        // imageview= (ImageView) findViewById(R.id.image);
         String url = "http://imgsrc.baidu.com/forum/w%3D580/sign=c49c299a17ce36d3a20483380af23a24/213fb80e7bec54e7a9f8cf5ebb389b504fc26a5e.jpg";
 
         imageview.setScaleType(ImageView.ScaleType.CENTER);
@@ -71,12 +93,43 @@ public class MainActivity extends Activity {
         Glide
                 .with(this)
                 .load(url)
-                        //                .placeholder(R.drawable.ic_launcher) //设置占位图
-                        //                .error(R.drawable.ic_launcher) //设置错误图片
+                //                .placeholder(R.drawable.ic_launcher) //设置占位图
+                //                .error(R.drawable.ic_launcher) //设置错误图片
                 .crossFade() //设置淡入淡出效果，默认300ms，可以传参
                 .bitmapTransform(new CropCircleTransformation(this))
 
                 .into(new ViewSimpleTarget(imageview));
+
+    }
+
+    @OnClick({R.id.bt_unvalid,R.id.bt_valid,R.id.image})
+    public void viewClick(View view){
+        switch (view.getId()){
+            case R.id.image:{
+                setEdgeTrackingEnabled(SwipeBackLayout.EDGE_ALL);
+                Intent intent=new Intent(context,MainActivity.class);
+                startActivity(intent);
+                break;
+            }
+            case  R.id.bt_unvalid:{
+                setSwipeBackEnable(false);
+             break;
+            }
+            case R.id.bt_valid:{
+                setSwipeBackEnable(true);
+                break;
+            }
+
+        }
+    }
+
+    @Override
+    protected void setListener() {
+
+    }
+
+    @Override
+    protected void processLogic(Bundle savedInstanceState) {
 
     }
 }
--- MyProject/libray_master/src/main/java/com/source/activity/BaseLibActivity.java
+++ MyProject/libray_master/src/main/java/com/source/activity/BaseLibActivity.java
@@ -5,7 +5,6 @@ import android.app.Activity;
 import android.graphics.Color;
 import android.os.Build;
 import android.os.Bundle;
-import android.support.v4.app.FragmentActivity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.Window;
@@ -13,18 +12,25 @@ import android.view.WindowManager;
 
 import com.readystatesoftware.systembartint.SystemBarTintManager;
 import com.source.common.AppManager;
+import com.source.util.CheckUtil;
+
+import me.imid.swipebacklayout.lib.SwipeBackLayout;
+import me.imid.swipebacklayout.lib.app.SwipeBackActivity;
 
 /**
  * Created by zhaoxu2014 on 15/9/19.
  */
-public abstract class BaseLibActivity extends FragmentActivity {
+public abstract class BaseLibActivity extends SwipeBackActivity {
     public Activity context;
     protected View mContentView;
-
+    private SwipeBackLayout mSwipeBackLayout;//左右划返回上一级页面
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         this.context=this;
+        super.onCreate(savedInstanceState);
         AppManager.getAppManager().addActivity(this);
+        initSwipeBack();
+
         // 修改状态栏颜色，4.4+生效
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
             setTranslucentStatus();
@@ -41,8 +47,40 @@ public abstract class BaseLibActivity extends FragmentActivity {
             setListener();
             processLogic(savedInstanceState);
         }
-        super.onCreate(savedInstanceState);
     }
+
+
+    public void setEdgeTrackingEnabled(int edgeFlags) {
+        if (!CheckUtil.isEmpty(mSwipeBackLayout))
+            mSwipeBackLayout.setEdgeTrackingEnabled(edgeFlags);
+    }
+    public void initSwipeBack() {
+        initSwipeBack(SwipeBackLayout.EDGE_LEFT);
+    }
+
+    public void initSwipeBack(int edgeFlags) {
+        mSwipeBackLayout = getSwipeBackLayout();
+        mSwipeBackLayout.setEdgeTrackingEnabled(edgeFlags);
+        setSwipeBackEnable(true);
+//外面可以做一下划动监听
+//        mSwipeBackLayout.addSwipeListener(new SwipeBackLayout.SwipeListener() {
+//            @Override
+//            public void onScrollStateChange(int state, float scrollPercent) {
+//
+//            }
+//
+//            @Override
+//            public void onEdgeTouch(int edgeFlag) {
+//                vibrate(VIBRATE_DURATION); //收到划动
+//            }
+//
+//            @Override
+//            public void onScrollOverThreshold() {
+//                vibrate(VIBRATE_DURATION);//划动结束
+//            }
+//        });
+    }
+
     @TargetApi(19)
     protected void setTranslucentStatus() {
         Window window = getWindow();
